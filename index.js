1956eedf1dc6910db854e03a4c6db2a881728c7d
Zlindsay26 <zlindsay260602@gmail.com>
2024-06-10 21:29:49 -0400

    new function made
    playRound function made

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const humanScore = 0;
+const computerScore = 0;
+
+
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length) + 1;
@@ -9,10 +14,11 @@ function getComputerChoice() {
     } if (randomChoice === 3) {
         return "Scissors"
     }
-    }
+    };
     
+
     function getHumanChoice() {
-   let answer = prompt("Rock, Paper, or Scissors?");
+   let answer = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
    if (answer === "Rock") {
     return "Rock";
@@ -23,6 +29,34 @@ function getComputerChoice() {
    } else {
     return "Please only use the given choices"
    }
+ };
+ const humanChoice = getHumanChoice();
+ const computerChoice = getComputerChoice();
+   function playRound(humanChoice, computerChoice) {
 
-    };
-    
+     if (humanChoice === "rock" && computerChoice === "Scissors"
+    || humanChoice === "paper" && computerChoice === "Rock"
+    || humanChoice === "scissors" && computerChoice === "Paper") {
+   
+   humanScore++;
+   console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === "rock" && computerChoice === "Paper"
+    || humanChoice === "paper" && computerChoice === "Scissors"
+    || humanChoice === "scissors" && computerChoice === "Rock") {
+
+        computerScore++;
+        console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`);
+    } else {
+        console.log("It's a tie! You both had the same choice!")
+    }
+};
+
+function playGame() {
+    playRound(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+    playRound(humanChoice, computerChoice);
+
+
+}
